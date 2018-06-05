3045f4722d7765acf42ea7db62e7a84a7f2c19de
René Nyffenegger <mail@renenyffenegger.ch>
2018-06-05 13:22:43 +0200

    + intl_id

--- tables/obj/key/keys-of-obj.sql
+++ tables/obj/key/keys-of-obj.sql
@@ -1,7 +1,8 @@
 select
   cky.name    key_name,
   oky.key_val key_value,
-  cky.descn   key_description
+  cky.descn   key_description,
+  cky.intl_id key_intl_id
 from
   obj_key      oky                      join
   code_obj_key cky on oky.obj_key_id = cky.id
