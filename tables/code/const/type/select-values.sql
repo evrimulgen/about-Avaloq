07b72f03b75552cc1b1bf5d0f97a99fdb6cc4e56
René Nyffenegger <mail@renenyffenegger.ch>
2018-06-06 13:41:19 +0200

    + code_const_id

--- tables/code/const/type/select-values.sql
+++ tables/code/const/type/select-values.sql
@@ -1,6 +1,7 @@
 select
   ctp.name       const_name,
-  cvl.name       value
+  cvl.name       value,
+  cvl.id         code_const_id
 from
   code_const_type ctp join
   code_const      cvl on cvl.const_type_id = ctp.id
