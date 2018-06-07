9446013c33f98e94c6d45162c24d748e8d59f3c5
René Nyffenegger <mail@renenyffenegger.ch>
2018-06-07 07:05:37 +0200

    + select.sql

---
+++ tables/code/obj/classif/type/select.sql
@@ -0,0 +1,14 @@
+select
+  id,
+  activ,
+  intl_id,
+  name
+from
+  code_obj_classif_type;
+--   
+--  ID ACTIV INTL_ID   NAME       
+-- --- ----- --------- -----------
+--   1 +     hist      hist       
+--   2 +     nohist    non hist   
+--   3 +     set       set        
+--   4 +     termd     termd      
