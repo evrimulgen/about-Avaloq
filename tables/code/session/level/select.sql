829e22456ccde7c213940215027d9f1fcae8bd16
René Nyffenegger <mail@renenyffenegger.ch>
2018-06-27 12:29:32 +0200

    + select.sql

---
+++ tables/code/session/level/select.sql
@@ -0,0 +1,15 @@
+select
+  id,
+  intl_id,
+  name
+from
+  code_session_level;
+--
+--        ID INTL_ID              NAME
+------------ -------------------- ----------------
+--         0 zero                 zero
+--         1 os                   os
+--         2 eod_file_dsp         eod_file_dsp
+--         3 eod_task_dsp         eod_task_dsp
+--         4 eod_job_dsp          eod_job_dsp
+--         5 open                 open
