c2c99748525efc97c9a8a1a4ca8357d5d3df196d
René Nyffenegger <mail@renenyffenegger.ch>
2018-07-17 07:25:38 +0200

    Update select stmt

--- tables/meta/msg/select.sql
+++ tables/meta/msg/select.sql
@@ -1,15 +1,40 @@
 select
-  met.id                  meta_msg_id,
---met.netw_id,
-  met.dir                 meta_msg_direction,
-  met.name                meta_msg_name,
-  net.id                  network_id,
-  net.name                network_name,
-  met.descn               meta_msg_description,
-  met.meta_msg_struct_id
+   mmg.id,
+-- mmg.netw_id,
+   net.name                        netw_name,
+   mmg.dir,
+   mmg.user_id,
+   mmg.intl_id,
+   mmg.name,
+   mmg.descn,
+   mmg.src_name                    msg_src_name,
+   str.src_name                    msg_struct_src_name,
+   str.intl_id                     msg_struct_intl_id,
+   mmg.meta_msg_gen_id,
+   mmg.hdr_def,
+   mmg.trl_def,
+   mmg.type,
+   mmg.settle_type,
+   mmg.src_pos_type,
+   mmg.target_pos_type,
+-- mmg.order_type_id,
+   odt.name                        order_type,
+-- mmg.meta_msg_struct_id
+   mmg.send_date_plan,
+   mmg.msg_status_start_id,
+   mmg.build_stmt,
+   mmg.hdl_stmt,
+   mmg.prcq_id,
+   mmg.is_ami,
+   mmg.activ,
+   mmg.doc_id_expr,
+   mmg.ld_doc,
+   mmg.is_doc_link_likely
 from
-  meta_msg   met   join
-  code_netw  net on met.netw_id = net.id
+   meta_msg           mmg                                     left join
+   code_netw          net on net.id = mmg.netw_id             left join
+   meta_msg_struct    str on str.id = mmg.meta_msg_struct_id  left join
+   code_order_type    odt on odt.id = mmg.order_type_id
 order by
-  net.name,
-  met.name;
+   net.activ nulls last,
+   net.name;
