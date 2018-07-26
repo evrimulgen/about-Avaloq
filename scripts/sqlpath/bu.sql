56ce9b08049f2efd335c8e025e4642f44db19494
René Nyffenegger <mail@renenyffenegger.ch>
2018-07-26 08:13:23 +0200

    + bu_id

--- scripts/sqlpath/bu.sql
+++ scripts/sqlpath/bu.sql
@@ -3,10 +3,12 @@
 --
 select
    substrb(nam.name, 1, 30)    bu_name,
-   substrb(bst.name, 1, 30)    bu_status
+   substrb(bst.name, 1, 30)    bu_status,
+   obu.obj_id                  bu_id
 from
-   obj_bp_bu      obu                               join
+   obj_bp_bu      obu                                   join
    code_bu_status bst on bst.id     = obu.bu_status_id  join
    obj_name_intl  nam on obu.obj_id = nam.obj_id
 where
-   obu.obj_id = session#.session#bu_id;	
+   obu.obj_id = session#.session#bu_id
+;
